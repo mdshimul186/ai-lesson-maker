2b766c2e21c3d7be21f55679305c2a42a35f992e
Md Ashraful Alam <62536223+mdshimul186@users.noreply.github.com>
2025-05-08 19:19:39 +0600

    updated

--- Frontend/src/utils/request.ts
+++ Frontend/src/utils/request.ts
@@ -8,7 +8,7 @@ interface RequestConfig {
     params?: object;
 }
 
-let baseUrl = 'http://127.0.0.1:8000';
+let baseUrl = 'http://96.85.103.133:8000';
 
 export function request<T>(config: RequestConfig): Promise<T> {
     return new Promise((resolve, reject) => {
