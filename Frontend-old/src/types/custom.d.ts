f70879d6b9cb6ca6d293b58fc70f74e07bc6a9a8
mdshimul186 <62536223+mdshimul186@users.noreply.github.com>
2025-06-06 01:55:02 +0600

    version updated

--- Frontend-old/src/types/custom.d.ts
+++
@@ -1,3 +0,0 @@
-// Type declarations for @hello-pangea/dnd
-
-declare module '@hello-pangea/dnd';
--- Frontend-old/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export function getSelectVoiceList(lang: string, list: string[]): string[] {
-    return list.filter((voice) => voice.startsWith(lang));
-}
